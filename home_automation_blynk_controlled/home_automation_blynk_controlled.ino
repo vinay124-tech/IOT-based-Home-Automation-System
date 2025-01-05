e02e1fafa728a61a05199ec95f998e0ebca80cfb
Manohar vinay Mududundi <123316843+vinay124-tech@users.noreply.github.com>
2025-01-05 21:26:12 +0530

    Update home_automation_blynk_controlled.ino

--- home_automation_blynk_controlled/home_automation_blynk_controlled.ino
+++ home_automation_blynk_controlled/home_automation_blynk_controlled.ino
@@ -6,9 +6,9 @@ Pheripherals  :   Arduino UNO, Temperature system, LED, LDR module, Serial Tank,
 
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID "TMPL3ixqwQhR-"
+#define BLYNK_TEMPLATE_ID "Enter your blynk template ID here"
 #define BLYNK_TEMPLATE_NAME "Home Automation"
-#define BLYNK_AUTH_TOKEN "jd9jcZuqk4WDbwNDpchr15xuZtyMxp47"
+#define BLYNK_AUTH_TOKEN "enter your blynk authentication token here"
 
 // Comment this out to disable prints 
 //#define BLYNK_PRINT Serial
